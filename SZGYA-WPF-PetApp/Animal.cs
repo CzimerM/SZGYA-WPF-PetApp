562529cb3badc48390d626114af662d3223fe4d8
Csizmadia Márk <airamek@teamorange.hu>
2024-05-15 10:03:07 +0200

    Add project files.

---
+++ SZGYA-WPF-PetApp/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_WPF_PetApp
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public string ImgPath { get; set; } 
+        public Animal(string line) 
+        {
+            string[] data = line.Split(';');
+            Name = data[0];
+            Age = int.Parse(data[1]);
+            Color = data[2];
+            ImgPath = data[3];
+        }
+    }
+}
---
+++ SZGYA-WPF-PetApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SZGYA_WPF_PetApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Animal> animals = new List<Animal>();
+
+        public MainWindow()
+        {
+            var sr = new StreamReader("../../../src/animals.txt");
+            while (!sr.EndOfStream) 
+            { 
+                animals.Add(new Animal(sr.ReadLine()));
+            }
+            sr.Close();
+            animals.Sort(a => a.Name);
+            InitializeComponent();
+        }
+    }
+}
